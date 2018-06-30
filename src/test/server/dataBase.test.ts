dc3373953cb5551c65889a5c486c66cba837d54e
Abe Ge <yige@microsoft.com>
2018-06-30 17:45:40 +0800

    update test

--- src/test/server/dataBase.test.ts
+++ src/test/server/dataBase.test.ts
@@ -1,12 +1,11 @@
 import { DataBase } from "../../Service/dataBase";
-
-
+const green: string = "\x1b[32m%s\x1b[0m";
 describe('jasmine test suite', () => {
     it('sample service test sayHi()', () => {
         let testDatabase = new DataBase(`${__dirname}/userInfo.json`);
         let actualResult = testDatabase.readUser("t002");
         actualResult.then(user=>{
-            console.info("\x1b[32m%s\x1b[0m",
+            console.info(green,
                 `the actual result is ${JSON.stringify(user)}`);
             expect(user.alias).toBe("t002");
             expect(user.name).toBe("tester002");
