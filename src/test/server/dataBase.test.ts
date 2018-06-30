6a3c4e2acf2d31c44060cd8a58d111ef9b4b8625
Abe Ge <yige@microsoft.com>
2018-06-30 17:23:02 +0800

    fix UT bug

--- src/test/server/dataBase.test.ts
+++ src/test/server/dataBase.test.ts
@@ -4,8 +4,10 @@ import { DataBase } from "../../Service/dataBase";
 describe('jasmine test suite', () => {
     it('sample service test sayHi()', () => {
         let testDatabase = new DataBase(`${__dirname}/userInfo.json`);
-        let actualResult = testDatabase.readUser("test002");
+        let actualResult = testDatabase.readUser("t002");
         actualResult.then(user=>{
+            console.info("\x1b[32m%s\x1b[0m",
+                `the actual result is ${JSON.stringify(user)}`);
             expect(user.alias).toBe("t002");
             expect(user.name).toBe("tester002");
             expect(user.password).toBe("password@456");
